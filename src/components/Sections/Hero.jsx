992cb44d76ec8900f1bd24f578cd26e3c825484f
Gopikrishnan Rajeev <gopikrishnan.rmg@live.in>
2025-10-09 19:36:04 -0400

    Fixed layout

--- src/components/Sections/Hero.jsx
+++ src/components/Sections/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     };
 
     return (
-        <section id='hero' className='flex flex-col md:flex-row justify-evenly pt-16 md:pt-16 h-screen'>
+        <section id='hero' className='flex flex-col md:flex-row justify-evenly pt-16 h-screen'>
             <div className='absolute inset-0'>
                 <LiquidEther
                     colors={['#5227FF', '#FF9FFC', '#B19EEF']}
@@ -21,46 +21,43 @@ const Hero = () => {
                     isBounce={false}
                     autoDemo={true}
                     autoSpeed={0.5}
-                    autoIntensity={0.5}
+                    autoIntensity={0.7}
                     takeoverDuration={0.25}
                     autoResumeDelay={3000}
                     autoRampDuration={0.6}
                 />
             </div>
 
-            <div className='flex flex-1 flex-center'>
-                <div className='text-2xl md:text-5xl space-y-2 text-center'>
-                    <SplitText
-                        text='Hello 👋🏻,'
-                        delay={100}
-                        duration={0.6}
-                        ease='power3.out'
-                        splitType='chars'
-                        from={{ opacity: 0, y: 40 }}
-                        to={{ opacity: 1, y: 0 }}
-                        threshold={0.1}
-                        rootMargin='-100px'
-                        textAlign='center'
-                        className='text-white'
-                        onLetterAnimationComplete={handleAnimationComplete}
-                    />
+            <div className='flex flex-1 flex-col flex-center text-2xl md:text-4xl space-y-2 text-center'>
+                <SplitText
+                    text='Hello 👋🏻,'
+                    delay={100}
+                    duration={0.6}
+                    ease='power3.out'
+                    splitType='chars'
+                    from={{ opacity: 0, y: 40 }}
+                    to={{ opacity: 1, y: 0 }}
+                    threshold={0.1}
+                    rootMargin='-100px'
+                    textAlign='center'
+                    className='text-white'
+                    onLetterAnimationComplete={handleAnimationComplete}
+                />
 
-                    <SplitText
-                        text={'I\'m Gopikrishnan Rajeev'}
-                        delay={70} // stagger start a bit later if you want
-                        duration={0.6}
-                        ease='power3.out'
-                        splitType='chars'
-                        from={{ opacity: 0, y: 40 }}
-                        to={{ opacity: 1, y: 0 }}
-                        threshold={0.1}
-                        rootMargin='-100px'
-                        textAlign='center'
-                        className='text-cyan-500'
-                        onLetterAnimationComplete={handleAnimationComplete}
-                    />
-                </div>
-
+                <SplitText
+                    text={'I\'m Gopikrishnan Rajeev'}
+                    delay={70} 
+                    duration={0.6}
+                    ease='power3.out'
+                    splitType='chars'
+                    from={{ opacity: 0, y: 40 }}
+                    to={{ opacity: 1, y: 0 }}
+                    threshold={0.1}
+                    rootMargin='-100px'
+                    textAlign='center'
+                    className='text-cyan-500'
+                    onLetterAnimationComplete={handleAnimationComplete}
+                />
             </div>
             <div className='flex flex-1 flex-center'>g</div>
         </section>
